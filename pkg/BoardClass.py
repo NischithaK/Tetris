e4244e49e169196d846fa1926e948399107973b2
sudheer1995 <sudheerachary115@gmail.com>
2016-11-27 01:16:09 +0530

    refactored and added methods

--- pkg/BoardClass.py
+++ pkg/BoardClass.py
@@ -1,37 +1,71 @@
-import pygame, sys, os, time
-from pygame.locals import *
+"""This module initialises & implements all the functionalities
+    of a tetris game board"""
 
-class Board:
+import pygame
 
-    def __init__(self,h,w):                 #initialise all the board variables like cell size , height ,
-        pygame.init()                       # width , screen window for the game
+from pygame.locals import Rect
+
+
+class Board(object):
+
+    """ This class has all the methods to initialise
+        & implement all the functionalities of a board"""
+
+    def __init__(self, h, w):
+
+        """initialise all the board variables like cell size, height,
+            width, screen window for the game"""
+
         self.board = []
         self.height = h
         self.width = w
         self.board = [[" " for j in range(self.width)]for i in range(self.height)]
-        self.screen = pygame.display.set_mode((self.height*20+40,self.width*20-40),0,32)
+
+    def initialise(self):
+
+        """ This initialises the game board with graphics rendered """
+
+        self.screen = pygame.display.set_mode((self.height * 20+40,
+                                               self.width * 20-40))
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
-        self.background.fill((0,105,125))
+        self.background.fill((0, 105, 125))
 
-    def fillPiecePos(self,color):           # colour the pieces of all filled pieces with the
-        self.screen.lock()                  # corresponding colour
+    def fillpiecepos(self, color):
+
+        """This method colours the pieces of all
+            filled pieces with the corresponding color"""
+
+        self.screen.lock()
         for i in range(self.height):
             for j in range(self.width):
                 if self.board[i][j] == "s" or self.board[i][j] == "m":
-                    pygame.draw.rect(self.screen,color,Rect((j*20,i*20),(18,18)))
+                    pygame.draw.rect(self.screen, color,
+                                     Rect((j * 20, i * 20),
+                                          (18, 18))
+                                    )
         pygame.display.update()
         self.screen.unlock()
 
-    def removePrevPieces(self):                # clear up all the previous positions
-        for i in range(self.height):            # of the block and moves to next position
-            for j in range(self.width):         # based in given instruction
+    def removeprevpieces(self):
+
+        """ This method clears up all the previous positions
+            of the block and moves to next position
+            based in given instruction """
+
+        for i in range(self.height):
+            for j in range(self.width):
                 if self.board[i][j] == "m":
                     self.board[i][j] = " "
 
-    def checkPiecePos(self,block_obj,fig,config):   # checks for the movement of the block
-        for elem in block_obj.drawFig(fig,config):  # for all sub blocks if any locks or not
+    def checkpiecepos(self, block_obj, fig, config):
+
+        """ This method checks for the movement of the block
+            for all sub blocks if any locks or not"""
+
+        for elem in block_obj.drawfig(fig, config):
             row, col = elem
-            if self.board[row+1][col] == "s":
+            if self.board[row + 1][col] == "s":
                 return 0
+
         return 1
